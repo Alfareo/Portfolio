1b502dbbc66206409ff53a2ed6bbfa1852566a2b
Edgar edwardo Lopez Falcon <Edgar edwardo Lopez Falcon>
2017-02-19 16:37:29 -0600

    Page working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var router = express.Router();
 
 app.set('port', (process.env.PORT || 8081));
 
@@ -7,14 +9,24 @@ app.set('port', (process.env.PORT || 8081));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
 //Render views - views is directory for all template files
 app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response) {
-  response.render('index');
+router.get('/', function(request,response){
+  response.render('index'); 
 });
+
+router.get('/elements', function(request,response){
+  response.render('elements'); 
+});
+
+router.get('/proto', function(request,response){
+  response.send('srenko');
+});
+
+app.use('/', router);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
